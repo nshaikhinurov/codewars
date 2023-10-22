ef6a4a99f36bb1abc90fcbd3252fb122afc5aba9
Nail Shaikhinurov <n.shaikhinurov@gmail.com>
2023-10-22 17:04:35 +0800

    CW-002: add Make The Deadfish Swim solution

---
+++ src/kyu6/MakeTheDeadfishSwim.ts
@@ -0,0 +1,25 @@
+// https://www.codewars.com/kata/51e0007c1f9378fa810002a9/train/typescript
+
+const commands: Record<string, (v: number, result: number[]) => number> = {
+  i: (v: number) => v + 1,
+  d: (v: number) => v - 1,
+  s: (v: number) => v * v,
+  o: (v: number, result: number[]) => {
+    result.push(v);
+    return v;
+  },
+};
+
+/** return the output array and ignore all non-op characters */
+export function parse(data: string): number[] {
+  let v = 0;
+  const result: number[] = [];
+
+  for (const command of data) {
+    if (command in commands) {
+      v = commands[command](v, result);
+    }
+  }
+
+  return result;
+}
